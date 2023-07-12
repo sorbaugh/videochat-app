6e21e496d68b3bc4dfbeb625e2fadda7ca1fe2f0
Stephan <stephan_orbaugh@outlook.de>
2023-07-12 06:54:06 +0200

    add nodemon and socket.io logic

--- app.js
+++ app.js
@@ -5,12 +5,31 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '/public/index.html');
 })
+
+let connectedPeers = [];
+
+io.on('connection', (socket) => {
+    connectedPeers.push(socket.id);
+    console.log(connectedPeers);
+
+    socket.on('disconnect', () => {
+        console.log("user diconnected");
+
+        const newConnectedPeers = connectedPeers.filter((peerSocketId) => {
+            return peerSocketId !== socket.id;
+        })
+
+        connectedPeers = newConnectedPeers;
+        console.log(connectedPeers);
+    })
+});
 
 server.listen(PORT, () => {
     console.log('listening on ${PORT}')
